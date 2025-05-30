d80886311a37104a6f655eeb07bb9a50216be272
Manuel-Espinosa <manuespinosadev@gmail.com>
2025-05-29 23:20:08 -0600

    allow cors policy

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
 from flask import Flask
+from flask_cors import CORS
 from src.routes.dog_routes import dog_blueprint
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 app.register_blueprint(dog_blueprint)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
